cb5746ae1aace896a5e68866b9fec40649c09685
noel <novocorboudnoel@gmail.com>
2025-05-17 21:36:10 +0200

    test

--- FriendsNetwork.Application/obj/Debug/net8.0/FriendsNetwork.Application.AssemblyInfo.cs
+++ FriendsNetwork.Application/obj/Debug/net8.0/FriendsNetwork.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FriendsNetwork.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fedab9d53274ca2c6fa02e876da2f2b23af02147")]
 [assembly: System.Reflection.AssemblyProductAttribute("FriendsNetwork.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FriendsNetwork.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FriendsNetwork.Tests/User/UserServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using FriendsNetwork.Application.Services.Users;  // Adjust namespaces
+using FriendsNetwork.Domain.Entities;
+using FriendsNetwork.Domain.Abstractions.Repositories;
+using FriendsNetwork.Domain.Abstractions.Services.Security; // Assuming IUserRepository here
+
+namespace FriendsNetwork.Tests
+{
+    [TestFixture]
+    public class UserServiceTests
+    {
+        private Mock<IUserRepository> _userRepositoryMock;
+        private Mock<IPasswordHasher> _passwordHasherMock;
+        private CreateUserService _createUserService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _createUserService = new CreateUserService(
+                _userRepositoryMock.Object,
+                _passwordHasherMock.Object
+                );
+        }
+
+        [Test]
+        public void CreateUserServiceAsync_ShouldThrow_WhenPasswordIsNullOrWhitespace()
+        {
+            // Arrange
+            string? username = "johndoe";
+            string? password = " ";
+
+            // Act & Assert
+            var ex = Assert.ThrowsAsync<ArgumentException>(() =>
+                _createUserService.CreateUserServiceAsync(username, password));
+
+            Assert.AreEqual("Password is required.", ex?.Message);
+            _passwordHasherMock.Verify(h => h.HashPassword(It.IsAny<string>()), Times.Never);
+            _userRepositoryMock.Verify(r => r.Add(It.IsAny<User>()), Times.Never);
+        }
+    }
+}
